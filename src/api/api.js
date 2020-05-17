4fa08bd752e6d37fe90caee38369adf082151835
LeoCR <leonardoas2007@gmail.com>
2020-05-16 20:46:00 -0600

    Unit Test - Testing getDessertsActions with moxios,redux-thunk and redux-mock-store

--- src/api/api.js
+++ src/api/api.js
@@ -1,5 +1,7 @@
 import axios from 'axios';
+import https from 'https';
 export default axios.create({
     baseURL:'https://localhost:49658',
-    responseType: 'json'
-})
+    responseType: 'json' 
+  })
+https.globalAgent.options.rejectUnauthorized = false;
