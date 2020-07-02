a545c97bac186888a0d97a24d96cc47fd2c14f7d
LeoCR <leonardoas2007@gmail.com>
2020-07-02 16:26:34 -0600

    Converted App.js Container to functional Component

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React from 'react';
 import Header from "../components/Header";
 import AddStrongDish from "../components/add/AddStrongDish";
 import AddEntree from "../components/add/AddEntree";
